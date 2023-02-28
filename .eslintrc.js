b86b5e12ddb9cdd5a08d4cb37fc326ed3287dbdb
Dmitry Shutov <dmitry.s@beda.software>
2023-02-28 13:27:58 +0700

    Initial project config

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    extends: [
+        'prettier',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'import'],
+    root: true,
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    rules: {
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal', ['index', 'sibling', 'parent']],
+                'newlines-between': 'always',
+                pathGroupsExcludedImportTypes: ['builtin'],
+                pathGroups: [
+                    {
+                        pattern: 'aidbox-react/**',
+                        group: 'external',
+                        position: 'after',
+                    },
+                    {
+                        pattern: 'src/**',
+                        group: 'internal',
+                        position: 'after',
+                    },
+                ],
+                alphabetize: { order: 'asc', caseInsensitive: true },
+            },
+        ],
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    arrowParens: 'always',
+    printWidth: 100,
+};
