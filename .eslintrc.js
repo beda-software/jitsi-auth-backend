db0eda7767104e50416a788c27b199f6bdde9eb7
Dmitry Shutov <dmitry.s@beda.software>
2023-03-06 11:43:35 +0700

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,37 @@
 module.exports = {
-    extends: [
-        'prettier',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'import'],
-    root: true,
-    parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname,
-    },
-    rules: {
-        'no-restricted-imports': ['error', { patterns: ['aidbox-react/src'] }],
-        'import/order': [
-            'error',
-            {
-                groups: ['builtin', 'external', 'internal', ['index', 'sibling', 'parent']],
-                'newlines-between': 'always',
-                pathGroupsExcludedImportTypes: ['builtin'],
-                pathGroups: [
-                    {
-                        pattern: 'aidbox-react/**',
-                        group: 'external',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'src/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                ],
-                alphabetize: { order: 'asc', caseInsensitive: true },
-            },
+  env: {
+    browser: true,
+  },
+  root: true,
+  extends: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  globals: {
+    node: true,
+  },
+  rules: {
+    'prettier/prettier': 0,
+    'no-restricted-imports': ['error', { patterns: ['aidbox-react/src'] }],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['index', 'sibling', 'parent']],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        pathGroups: [
+          {
+            pattern: 'shared/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'src/**',
+            group: 'internal',
+            position: 'after',
+          },
         ],
-    },
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
 };
